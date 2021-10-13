b22f1b38c49802c95dee888edd46aba3341a9cbd
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-14 02:30:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# example of list in python
+
+myList = [9, 'hello', 2, 'python']
+
+print(myList[0]) # output --> 9
+print(myList[-3]) # output --> hello
+print(myList[:3]) # output --> [9, 'hello', 2]
+print(myList) # output --> [9, 'hello', 2, 'python']
